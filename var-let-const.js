99384a5dc229afc1369d75db8e7aec4f623cd386
Maimun54 <maimunislam@gmail.com>
2025-01-02 12:16:30 +0600

    Update

--- var-let-const.js
+++ var-let-const.js
@@ -1,10 +1,6 @@
-// var let const 
-//var : no reason to use var
-//let : allow it to reassign
-// Const: do not allow it to reassign
 
 const teamM = 5;
-//teamM = 10;
+
 const IncreseTeamMember = teamM + 5
 console.log(IncreseTeamMember);
 
